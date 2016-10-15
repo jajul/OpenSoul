0d690b43f0b7aafb7b6a6cc143ce8c4c1e17409a
Gosha Chmutin <gochan@mail.ru>
2016-10-16 01:28:37 +0300

    добавлены вертушки

--- src/main/webapp/js/func.js
+++ src/main/webapp/js/func.js
@@ -3,32 +3,55 @@
  */
 var question = null;
 var state = {
-    flow: 'start' // next, finish, sent
+    flow: 'start', // load_question, next, finish, load_sending, sent
 };
+var loading = '<img class="small-loading" src="img/loading.gif"/>';
 
-$(document).ready(render);
+$(document).ready(startRender);
 
-function render(){
-    $('#question_num').toggle( state.flow=='next' );
-    if(state.flow=='next'){
+function startRender(){
+    render('start');
+}
+
+function render(flow){
+    if(flow){
+        state.flow = flow;
+    }
+
+    $('#question_num').toggle( state.flow=='next' || state.flow=='load_question' );
+    if(state.flow=='load_question'){
+        toggleLoading($('#question_num'));
+    }
+    else if(state.flow=='next'){
         $('#question_num').show().html('Question ' + question.num);
-        $('#question_text').show().html(question.text);
     }
 
     $('#question_text').toggle( state.flow!='start' );
-    if(state.flow=='next'){
+    if(state.flow=='load_question'){
+        toggleLoading($('#question_text'));
+    }
+    else if(state.flow=='next'){
         $('#question_text').show().html(question.text);
     }
     else if(state.flow=='finish'){
         $('#question_text').html('Thank you!');
     }
     $('#startButton').toggle( state.flow=='start' );
-    $('#nextButton').toggle( state.flow=='next' );
+    $('#nextButton').toggle( state.flow=='next' || state.flow=='load_question' );
     $('#sendButton').toggle( state.flow=='finish' );
     $('#sendSuccess').toggle( state.flow=='sent' );
+    $('#loadingButton').toggle( state.flow=='load_sending' );
+
+    if(state.flow=='load_question' || state.flow=='load_sending' ){
+        $('.btn').prop('disabled', true);
+    }
+    else{
+        $('.btn').prop('disabled', false);
+    }
 }
 
 function get_question() {
+    render('load_question');
     $.ajax({
         url: '/get_question',
         data: {num: (question!=null ? question.num + 1 : 1)},
@@ -52,25 +75,25 @@ function setQuestion(num, text) {
 function printQuestion() {
     if (question == null) {
         // Начало теста - отображаем кнопку Старт
-        state.flow = 'start';
+        render('start');
     }
     else if (question.num > 0) {
         if (question.num == 1) {
             createCall();
         }
         // Отображаем следующий вопрос
-        state.flow = 'next';
+        render('next');
     }
     else if (question.num == -1) {
         // Конец теста
         disconnectCall();
-        state.flow = 'finish';
+        render('finish');
     }
-    render();
 }
 
 
 function send_result() {
+    render('load_sending');
     $.ajax({
         url: '/send_result',
         data: {
@@ -87,4 +110,8 @@ function send_result() {
             console.error(xhr);
         }
     });
+}
+
+function toggleLoading($elem){
+    $elem.show().html($('<img class="small-loading" src="img/loading.gif"/>'));
 }
