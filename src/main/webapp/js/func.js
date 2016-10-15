2fc62486c8cd04ebbb757da217669dafdfcd06bd
Gosha Chmutin <gochan@mail.ru>
2016-10-15 22:29:05 +0300

    func.js изменен на render()

--- src/main/webapp/js/func.js
+++ src/main/webapp/js/func.js
@@ -1,14 +1,37 @@
 /**
  * Created by Gochan on 15.10.2016.
  */
-var question = {num: 0, text: ''};
+var question = null;
+var state = {
+    flow: 'start' // next, finish, sent
+};
 
-$(document).ready(printQuestion);
+$(document).ready(render);
+
+function render(){
+    $('#question_num').toggle( state.flow=='next' );
+    if(state.flow=='next'){
+        $('#question_num').show().html('Question ' + question.num);
+        $('#question_text').show().html(question.text);
+    }
+
+    $('#question_text').toggle( state.flow!='start' );
+    if(state.flow=='next'){
+        $('#question_text').show().html(question.text);
+    }
+    else if(state.flow=='finish'){
+        $('#question_text').html('Thank you!');
+    }
+    $('#startButton').toggle( state.flow=='start' );
+    $('#nextButton').toggle( state.flow=='next' );
+    $('#sendButton').toggle( state.flow=='finish' );
+    $('#sendSuccess').toggle( state.flow=='sent' );
+}
 
 function get_question() {
     $.ajax({
         url: '/get_question',
-        data: {num: question.num + 1},
+        data: {num: (question!=null ? question.num + 1 : 1)},
         dataType: 'json',
         success: function (data) {
             setQuestion(data.num, data.text);
@@ -17,34 +40,28 @@ function get_question() {
 }
 
 function setQuestion(num, text) {
+    question = {};
     question.num = num;
     question.text = text;
     printQuestion();
 }
 
 function printQuestion() {
-    if (question.num > 0) {
-        $('#question_num').show().html('Question ' + question.num);
-        $('#question_text').show().html(question.text);
-        $('#startButton').hide();
-        $('#nextButton').show();
-        $('#sendButton').hide();
+    if (question == null) {
+        // Начало теста - отображаем кнопку Старт
+        state.flow = 'start';
     }
-    else if (question.num == 0) {
-        $('#question_num').hide();
-        $('#question_text').hide();
-        $('#startButton').show();
-        $('#nextButton').hide();
-        $('#sendButton').hide();
+    else if (question.num > 0) {
+        // Отображаем следующий вопрос
+        state.flow = 'next';
     }
     else if (question.num == -1) {
-        $('#question_num').hide();
-        $('#question_text').html('Thank you!');
-        $('#startButton').hide();
-        $('#nextButton').hide();
-        $('#sendButton').show();
+        // Конец теста
+        state.flow = 'finish';
     }
+    render();
 }
+
 
 function send_result() {
     $.ajax({
@@ -56,6 +73,8 @@ function send_result() {
         },
         dataType: 'json',
         success: function (data) {
+            state.flow = 'sent';
+            render();
         }
     });
 }
